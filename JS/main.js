092bb37a7f618e402500420691f7cacff7bbbc3c
Leandro Agustín Gigena <gigelean@gmail.com>
2021-07-19 08:57:45 -0300

    cambios en main.js

--- JS/main.js
+++ JS/main.js
@@ -2,11 +2,16 @@ var capital=prompt("Introduzca el capital solicitado");
 var interes=prompt("Indique el interes anual o tasa");
 var anios=prompt("Introduzca el numero de años");
 
-var a=anios*12;
-var i=interes/1200;
+function calcularIntereses(cap, int, ani){
+    var a=ani*12;
+    var i=int/1200;
+    
+    
+    var factor = Math.pow(i+1,a);
+    var cuota= cap*i*factor/(factor-1);
+    
+    alert("La cuota de "+capital + " a " +a+ " meses y interés mensual de "+i*100+"% es " + cuota);
 
+}
 
-var factor = Math.pow(i+1,a);
-var cuota= capital*i*factor/(factor-1);
-
-alert("La cuota de "+capital + " a " +a+ " meses y interés mensual de "+i*100+"% es " + cuota);
+calcularIntereses(capital, interes, anios);
